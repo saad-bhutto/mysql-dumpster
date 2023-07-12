f0792ded9f02bf8323c2622622fc0ea5946213c9
saadbhutto <saad.bhutto@aroflo.com>
2023-07-12 16:35:28 +1000

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+All notable changes to `database-dump` will be documented in this file
+
+## 1.0.0 - 201X-XX-XX
+
+- initial release
---
+++ src/DatabaseDumpServiceProvider.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace SaadBhutto\DatabaseDump;
+
+use Illuminate\Support\ServiceProvider;
+
+class DatabaseDumpServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        /*
+         * Optional methods to load your package assets
+         */
+        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'database-dump');
+        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'database-dump');
+        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // $this->loadRoutesFrom(__DIR__.'/routes.php');
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/config.php' => config_path('database-dump.php'),
+            ], 'config');
+
+            // Publishing the views.
+            /*$this->publishes([
+                __DIR__.'/../resources/views' => resource_path('views/vendor/database-dump'),
+            ], 'views');*/
+
+            // Publishing assets.
+            /*$this->publishes([
+                __DIR__.'/../resources/assets' => public_path('vendor/database-dump'),
+            ], 'assets');*/
+
+            // Publishing the translation files.
+            /*$this->publishes([
+                __DIR__.'/../resources/lang' => resource_path('lang/vendor/database-dump'),
+            ], 'lang');*/
+
+            // Registering package commands.
+            $this->commands([
+
+            ]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        // Automatically apply the package configuration
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'database-dump');
+
+        // Register the main class to use with the facade
+        $this->app->singleton('database-dump', function () {
+            return new DatabaseDump;
+        });
+    }
+}
